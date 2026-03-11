efdda8107e4e619e18637d88b4e93cff10a2f581
kameshNethsara <kamesh317nethsara@gmail.com>
2026-03-11 09:50:17 +0530

    initial commit Project Initialized

---
+++ rest-countries-explorer-be/src/main/java/com/kns/restcountriesexplorerbe/RestCountriesExplorerBeApplication.java
@@ -0,0 +1,13 @@
+package com.kns.restcountriesexplorerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCountriesExplorerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCountriesExplorerBeApplication.class, args);
+    }
+
+}
---
+++ rest-countries-explorer-be/src/test/java/com/kns/restcountriesexplorerbe/RestCountriesExplorerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kns.restcountriesexplorerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCountriesExplorerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
